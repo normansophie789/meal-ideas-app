cb6852565d42545baf908adbf8a0c9489e296382
normansophie789 <sophie.norman2@btinternet.com>
2018-12-30 22:47:48 +0000

    Updates

---
+++ precache-manifest.1369699ee692f2a78f8c974869e85b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7885cad661af30fc4e766d33b6dafd1",
+    "url": "/meal-ideas-app/static/media/header-image.b7885cad.jpg"
+  },
+  {
+    "revision": "58e353e93b7a41dfea20",
+    "url": "/meal-ideas-app/static/js/runtime~main.58e353e9.js"
+  },
+  {
+    "revision": "1d5381e5bb236ac0c3d8",
+    "url": "/meal-ideas-app/static/js/main.1d5381e5.chunk.js"
+  },
+  {
+    "revision": "1d777166e58f14ec03a9",
+    "url": "/meal-ideas-app/static/js/1.1d777166.chunk.js"
+  },
+  {
+    "revision": "1d5381e5bb236ac0c3d8",
+    "url": "/meal-ideas-app/static/css/main.56b84117.chunk.css"
+  },
+  {
+    "revision": "fc3a0024c8f6adc3571ea5863dc89742",
+    "url": "/meal-ideas-app/index.html"
+  }
+];
