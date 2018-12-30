90f6fb2d02bc39f47198204496376eaa01f1b11c
normansophie789 <sophie.norman2@btinternet.com>
2018-12-30 14:47:47 +0000

    added underscore package and implementation to decode response

--- src/components/MealTile.js
+++ src/components/MealTile.js
@@ -3,15 +3,18 @@ A component to render each meal as a tile in the Meals component.
 */
 
 import React from 'react';
+import Underscore from 'underscore';
 
 export class MealTile extends React.Component {
     render() {
+        var title = this.props.title;
+        title = Underscore.unescape(title)
         return (
             <div class="meal_tile">
                 <a href={this.props.link}>
                     <img src={this.props.image_src} alt={this.props.title} />
                 </a>
-                <p>{this.props.title}</p>
+                <p>{title}</p>
             </div>
         );
     }
